014043559a45729a2ad368e97dc9a16739d317ea
2057559s <nik.saunderson@gmail.com>
2016-08-11 17:10:12 +0100

    string filter and number filter working

--- directives/addComponentFilterOperation.js
+++ directives/addComponentFilterOperation.js
@@ -6,14 +6,32 @@
     app.directive('addComponentFilterOperation', function() {
         return {
             restrict: "EA",
-            scope: {},
+            scope: {
+                dataset: "=dataset"
+            },
             templateUrl: 'templates/addComponentFilterOperation.html',
 
             controller: function($scope, ndms) {
                 $scope.form = {component:""};
-                $scope.components = ["person", "wage", "timestamp", "x"];
+                $scope.components = [];
 
                 $scope.TYPE = "";
+
+                $scope.$watch('dataset', function(dataset) {
+                    if (dataset===undefined) {
+                        return;
+                    }
+
+                    $scope.components.splice(0);
+                    for(comp in $scope.dataset[0]) {
+                        $scope.components.push(comp);
+//                        $scope.TYPE = typeof $scope.dataset[0][comp]
+                    }
+                });
+
+                $scope.$watch('form.component', function(component) {
+                    $scope.TYPE = typeof $scope.dataset[0][component]
+                });
 
                 $scope.add = function() {
                     var form = JSON.parse(JSON.stringify($scope.form));
@@ -33,7 +51,7 @@
                     switch($scope.TYPE) {
                         case "string":
                             return function(d) {
-                                return false;
+                                return form.text === d[form.component];
                             };
                         case "number":
                             return function(d) {
